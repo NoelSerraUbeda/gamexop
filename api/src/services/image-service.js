06b8b9776325d899a0de7134d7e9be44020a98de
NoelSerraUbeda <neweonj@gmail.com>
2024-03-07 15:43:14 +0100

    bucle

--- api/src/services/image-service.js
+++ api/src/services/image-service.js
@@ -5,9 +5,19 @@ const sharp = require('sharp')
 module.exports = class ImageService {
   uploadImage = async images => {
     console.log(images)
+    for (let i = 0; i < images.file.length; i++) {
+      const originalName = images.file[i].originalname
+      if (/\s+/.test(originalName)) {
+        const modifiedName = originalName.replace(/\s+/g, '-')
+        console.log(`Nombre: ${modifiedName}`)
+      } else {
+        console.log(`Nombre: ${originalName}`)
+      }
+    }
   }
 
-  resizeImages = async (images) => {
+  resizeImages = async (images, width, height) => {
+
   }
 
   deleteImages = async filename => {
